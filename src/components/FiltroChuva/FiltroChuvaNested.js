20fc98c297b05370caef7d0a854769fda834ce9a
Tiago Quadros <tiago.quadros@betrybe.com>
2022-08-29 17:51:22 -0300

    Adiciona filtro de Sort

--- src/components/FiltroChuva/FiltroChuvaNested.js
+++ src/components/FiltroChuva/FiltroChuvaNested.js
@@ -24,6 +24,12 @@ function FiltroChuvaNested() {
   // estado para o array de filtros
   const [selectedFilters, setSelectedFilters] = useState([]);
 
+  // estado para controlar a ordenação
+  const [order, setOrder] = useState({
+    column: 'CAPITAL',
+    direction: 'ASC'
+  });
+
   // vc quer saber mais sobre isso? Link legal aqui
   // https://devtrium.com/posts/async-functions-useeffect
 
@@ -59,81 +65,123 @@ function FiltroChuvaNested() {
     return bools.every((el) => el);
   };
 
+  const extractNumber = (string) => Number(string.replace(/\*/g, ''));
+
+  const ordenaDados = (a, b) => {
+    const { column, direction } = order;
+    if (column !== 'CAPITAL') {
+      if ( direction === 'ASC' ) {
+        return (extractNumber(a[column]) - extractNumber(b[column]));
+      } else if ( direction === 'DESC' ) {
+        console.log(extractNumber(a[column]));
+        console.log(extractNumber(b[column]));
+        return (extractNumber(a[column]) - extractNumber(b[column])) * -1;
+      }  
+    }
+    if ( direction === 'ASC' ) {
+      return (a[column] > b[column]) ? 1 : -1;
+    } else if ( direction === 'DESC' ) {
+      return (a[column] < b[column]) ? 1 : -1;
+    }
+  }
+
   const tratarOpcoes = (opcao) =>
     !selectedFilters.find((filtro) => opcao === filtro.column);
 
   return (
     <div>
       <header>
-        <div className="filter-city">
+        <div className="filter-box">
+          <div className="filter-city">
+            <input
+              placeholder="Filtro por cidade"
+              type="text"
+              id=""
+              name="cityName"
+              value={cityInput}
+              onChange={(e) => {
+                setNameInput(e.currentTarget.value);
+              }}
+            />
+          </div>
+          <select
+            value={selected.column}
+            onChange={(e) => setSelected({ ...selected, column: e.target.value })}
+          >
+            <option value="">Selecione uma coluna</option>
+            {['PMAX12', 'TMIN18', 'TMAX18'].filter(tratarOpcoes).map((column) => (
+              <option value={column} key={column}>
+                {column}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selected.condition}
+            onChange={(e) =>
+              setSelected({ ...selected, condition: e.target.value })
+            }
+          >
+            <option value="">Selecione uma condição</option>
+            <option value=">">MAIOR DO QUE</option>
+            <option value="<">MENOR DO QUE </option>
+            <option value="=">IGUAL </option>
+          </select>
           <input
-            placeholder="Filtro por cidade"
+            placeholder="Digite o valor"
             type="text"
             id=""
-            name="cityName"
-            value={cityInput}
-            onChange={(e) => {
-              setNameInput(e.currentTarget.value);
-            }}
+            name="filterValue"
+            value={selected.value}
+            onChange={(e) => setSelected({ ...selected, value: e.target.value })}
           />
+          <div className="buttons">
+            <button
+              className="add"
+              onClick={() => {
+                setSelectedFilters([...selectedFilters, selected]);
+                setSelected({
+                  column: '',
+                  condition: '',
+                  value: '',
+                });
+              }}
+            >
+              ADICIONAR
+            </button>
+            <button
+              className="clear"
+              onClick={() => {
+                setSelectedFilters([]);
+                setSelected({
+                  column: '',
+                  condition: '',
+                  value: '',
+                });
+              }}
+            >
+              LIMPAR
+            </button>
+          </div>
         </div>
-        <select
-          value={selected.column}
-          onChange={(e) => setSelected({ ...selected, column: e.target.value })}
-        >
-          <option value="">Selecione uma coluna</option>
-          {['PMAX12', 'TMIN18', 'TMAX18'].filter(tratarOpcoes).map((column) => (
-            <option value={column} key={column}>
-              {column}
-            </option>
-          ))}
-        </select>
-        <select
-          value={selected.condition}
-          onChange={(e) =>
-            setSelected({ ...selected, condition: e.target.value })
-          }
-        >
-          <option value="">Selecione uma condição</option>
-          <option value=">">MAIOR DO QUE</option>
-          <option value="<">MENOR DO QUE </option>
-          <option value="=">IGUAL </option>
-        </select>
-        <input
-          placeholder="Digite o valor"
-          type="text"
-          id=""
-          name="filterValue"
-          value={selected.value}
-          onChange={(e) => setSelected({ ...selected, value: e.target.value })}
-        />
-        <div className="buttons">
-          <button
-            className="add"
-            onClick={() => {
-              setSelectedFilters([...selectedFilters, selected]);
-              setSelected({
-                column: '',
-                condition: '',
-                value: '',
-              });
-            }}
+        <div className="filter-box">
+          <select
+            value={order.column}
+            onChange={(e) => setOrder({ ...order, column: e.target.value })}
           >
-            ADICIONAR
-          </button>
-          <button
-            className="clear"
-            onClick={() => {
-              setSelectedFilters([]);
-              setSelected({
-                column: '',
-                condition: '',
-                value: '',
-              });
-            }}
-          >
-            LIMPAR
-          </button>
+            {['CAPITAL', 'PMAX12', 'TMIN18', 'TMAX18'].map((column) => (
+              <option value={column} key={column}>
+                {column}
+              </option>
+            ))}
+          </select>
+          <label>
+            <input type="radio" name="orderDirection" value="ASC" checked={ order.direction === 'ASC' } onChange={({ target }) => setOrder({ ...order, direction: target.value })} />
+            Crescente
+          </label>
+          <label>
+            <input type="radio" name="orderDirection" value="DESC" checked={ order.direction === 'DESC' } onChange={({ target }) => setOrder({ ...order, direction: target.value })} />
+            Decrescente
+          </label>
         </div>
       </header>
       {selectedFilters.map((filter, index) => (
@@ -167,6 +215,7 @@ function FiltroChuvaNested() {
             el.CAPITAL.toLowerCase().includes(cityInput.toLowerCase())
           )
             .filter(tratarDados)
+            .sort(ordenaDados)
             .map((dados) => (
               <tr key={dados.CAPITAL}>
                 <td>{dados.CAPITAL}</td>
